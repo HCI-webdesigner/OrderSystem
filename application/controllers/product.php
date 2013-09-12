c7abe753f8e8b72437503595fbe6fcdd5ca89732
hhqcontinue <hhqcontinue@gmail.com>
2013-09-12 23:05:35 +0800

    new log

--- application/controllers/product.php
+++ application/controllers/product.php
@@ -29,20 +29,6 @@
 			$this->load->view('footer');
 		}
 
-		public function allProductNames() {
-			$producttype1 = $this->product_model->get_lv1AllNames();
-			$allProduct2Names = array();
-			foreach ($producttype1 as $row) {
-				$producttype2 = $this->product_model->get_lv2Names($row['id']);
-				array_push($allProduct2Names, $producttype2);
-			}
-			$header['pagename'] = '送货信息管理';
-			$header['allProduct2Names'] = $allProduct2Names;
-			$header['producttype1'] = $producttype1;
-
-			$this->load->view('header',$header);
-			$this->load->view($this->_view_url.'allProductNames');
-			$this->load->view('footer');
-		}
+		
 
 	}
--- application/views/system/product.php
+++ application/views/system/product.php
@@ -1,14 +1,16 @@
 	<?php echo anchor('/product/allProductNames/', '显示所有产品');?>
 
 	<?php foreach($producttype1 as $productType1Row):?>
+		<ul class="">
 		<h2><input type="button" name="getProduct_lv2" value="<?=$productType1Row['name']?>" onclick="showProductLv2Names(<?=$productType1Row['id']?>)"></h2>
 			<div id="productLv2Names<?=$productType1Row['id']?>" style="display:none">
 			<?php foreach($producttype2 as $productType2Row):?>
 			<?php foreach ($productType2Row as $row): ?>
 			<?php if($productType1Row['id'] == $row['PTid']):?>	
-			<h3><?=$row['name'];?></h3>
+			<li><h3><?=$row['name'];?></h3></li>
 			<?php endif?>
 			<?php endforeach?>
 			<?php endforeach?>
 			</div>
+		</ul>
 	<?php endforeach?>
